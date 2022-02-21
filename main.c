1c73e02ac0f0aa9e385e22f2ad4b6398a7d06804
DenisNomokonov <den.nomok@yandex.ru>
2022-02-21 17:16:36 +0700

    Add geometry

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+const float Pi = 3.1415926;
 int main()
 {
-    printf("Hello, World");
+    int f;
+    float a, b, c, r, p, p2, s;
+    printf("Your figure: \n 1: Circle \n 2: Triangle \n ");
+    scanf("%d", &f);
+    switch (f) {
+    case 1:
+        printf("Circle radius: \n");
+        scanf("%f", &r);
+        s = Pi * r * r;
+        p = 2 * Pi * r;
+        printf("Area circle = %.2f \n", s);
+        printf("Perimeter circle = %.2f \n", p);
+        break;
+    case 2:
+        printf("Triangle sides: \n");
+        scanf("%f %f %f", &a, &b, &c);
+        if (c <= a + b) {
+            p = a + b + c;
+            p2 = p / 2;
+            s = (sqrt(p2 * (p2 - a) * (p2 - b) * (p2 - c)));
+            printf("Area triangle = %.2f \n", s);
+            printf("Perimeter triangle = %.2f \n", p);
+            break;
+        } else
+            printf("Error");
+    }
     return 0;
 }
+
