c05a6fec5df66a9c230ffdda8df5826043dcd480
DenisNomokonov <den.nomok@yandex.ru>
2022-05-08 15:07:10 +0700

    Change main.c

--- src/main/main.c
+++ src/main/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "libmain/func.h"
 #include <ctype.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "libmain/func.h"
 
 int main(void)
 {
