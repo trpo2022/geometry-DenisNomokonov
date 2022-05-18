389ef6bc1e02ac9aac6314ce6dff2b008b225ce5
DenisNomokonov <den.nomok@yandex.ru>
2022-05-18 23:34:11 +0700

    Change cross test

--- test/tests.c
+++ test/tests.c
@@ -1,30 +1,26 @@
 #include <ctest.h>
+#include <libmain/cross.h>
 #include <libmain/func.h>
 #include <libmain/output.h>
 #include <libmain/scheck.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 float xk[2] = {0};
 float yk[2] = {0};
 float rad[2] = {0};
 
-CTEST(suite, cross)
+CTEST(suite, circle_cross)
 {
-    xk[0] = 2;
-    yk[0] = 2;
-    rad[0] = 2;
-    xk[1] = 2;
-    yk[1] = 2;
-    rad[1] = 2;
+    xk[0] = 0;
+    yk[0] = 0;
+    rad[0] = 1.5;
+    xk[1] = 0;
+    yk[1] = 0;
+    rad[1] = 1.5;
     int j = 0, c = 1, res = 0;
     int exp = 1;
-    for (int k = 0; k <= c; k++) {
-        if (k == j)
-            continue;
-        float d = fabs(sqrt(pow((xk[k] - xk[j]), 2) + pow((yk[k] - yk[j]), 2)));
-        if (d <= (rad[j] + rad[k]))
-            res++;
-    }
+    res = intersect(j, c, xk, yk, rad);
     ASSERT_EQUAL(exp, res);
 }
 
